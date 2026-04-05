743efca354a33c5d6b1d7a8f76d209ce56478c16
JgaravitoG <jgaravitog@unicartagena.edu.co>
2026-04-05 14:49:45 -0500

    Ejercio 2

---
+++ taller_0/Vehiculo.java
@@ -0,0 +1,11 @@
+class Vehiculo {
+    String marca;
+    String modelo;
+    int velocidadActual;
+
+    void mostrarEstado(){
+        System.out.println("Marca: "+marca);
+        System.out.println("Modelo:"+modelo);
+        System.out.println("Velocidad Actual: "+velocidadActual);
+    }
+}
