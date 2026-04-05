4b9bab367eef01f7f71ba238ae6b1f85b663efc0
JgaravitoG <jgaravitog@unicartagena.edu.co>
2026-04-05 16:57:29 -0500

    Ejercicio 6

---
+++ taller_0/Bloque B/Estudiante.java
@@ -0,0 +1,22 @@
+class Estudiante {
+    String nombre;
+    String codigo;
+    int semestre;
+
+    void mostrarInf(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Codigo: "+codigo);
+        System.out.println("Semestre: "+semestre);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Estudiante estudiante1 = new Estudiante();
+        estudiante1.nombre = "Juan";
+        estudiante1.codigo = "123123";
+        estudiante1.semestre = 2;
+        estudiante1.mostrarInf();
+    }
+    
+}
