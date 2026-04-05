b6a3da13ba5a91d43d52978b3ac29712eaa3f7eb
JgaravitoG <jgaravitog@unicartagena.edu.co>
2026-04-05 18:59:41 -0500

    Ejercicio 12

--- taller_0/Bloque C/Vehiculo.java
+++ taller_0/Bloque C/Vehiculo.java
@@ -6,9 +6,32 @@ class Vehiculo {
     void mostrarEstado(){
         System.out.println("Marca: "+marca);
         System.out.println("Modelo:"+modelo);
-        System.out.println("Velocidad Actual: "+velocidadActual);
+        System.out.println("Velocidad Actual: "+velocidadActual+ " km/h");
     }
     void acelerar(){
         velocidadActual += 10;
+    }
+    void frenar(){
+        if (velocidadActual >=10) {
+            velocidadActual -= 10;
+        } 
+        else{
+            velocidadActual -= velocidadActual;
+        }
+    }
+        public static void main(String[] args) {
+        Vehiculo vehiculo1 = new Vehiculo();
+        Vehiculo vehiculo2 = new Vehiculo();
+        vehiculo1.marca = "Tesla";
+        vehiculo1.modelo = "Model 3";
+        vehiculo1.velocidadActual = 115;
+        vehiculo2.marca = "Ford";
+        vehiculo2.modelo = "F-150";
+        vehiculo2.velocidadActual = 40;
+
+        System.out.println("--Estado vehiculos--");
+        vehiculo1.mostrarEstado();
+        System.out.println("---------");
+        vehiculo2.mostrarEstado();
     }
 }
