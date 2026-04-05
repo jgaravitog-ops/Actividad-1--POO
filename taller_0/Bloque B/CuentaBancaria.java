094453511f81b12beb253cb0426f25ea5357518c
JgaravitoG <jgaravitog@unicartagena.edu.co>
2026-04-05 18:34:01 -0500

    Ejercicio 10

--- taller_0/Bloque B/CuentaBancaria.java
+++ taller_0/Bloque B/CuentaBancaria.java
@@ -1,7 +1,7 @@
 class CuentaBancaria {
     int numero;
     String titular;
-    float saldo;
+    double saldo;
 
     void mostrarCuenta(){
         System.out.println("Numero: "+numero);
@@ -9,4 +9,20 @@ class CuentaBancaria {
         System.out.println("Saldo: "+saldo);
     }
     
+    public static void main(String[] args) {
+        CuentaBancaria cBancaria1 = new CuentaBancaria();
+        CuentaBancaria cBancaria2 = new CuentaBancaria();
+
+        cBancaria1.numero = 123;
+        cBancaria1.titular = "Pedro Gonzales";
+        cBancaria1.saldo = 7769;
+
+        cBancaria2.numero = 321;
+        cBancaria2.titular = "Jose Garcia";
+        cBancaria2.saldo = 5243.5;
+
+        cBancaria1.mostrarCuenta();
+        System.out.println("----------");
+        cBancaria2.mostrarCuenta();
+    }
 }
