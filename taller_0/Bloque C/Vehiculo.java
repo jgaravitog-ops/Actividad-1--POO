afdd5e075c8df40dc02e367849f040fb387f0dc3
JgaravitoG <jgaravitog@unicartagena.edu.co>
2026-04-05 18:44:31 -0500

    Ejercicio 11

---
+++ taller_0/Bloque C/Vehiculo.java
@@ -0,0 +1,14 @@
+class Vehiculo {
+    String marca;
+    String modelo;
+    int velocidadActual;
+
+    void mostrarEstado(){
+        System.out.println("Marca: "+marca);
+        System.out.println("Modelo:"+modelo);
+        System.out.println("Velocidad Actual: "+velocidadActual);
+    }
+    void acelerar(){
+        velocidadActual += 10;
+    }
+}
