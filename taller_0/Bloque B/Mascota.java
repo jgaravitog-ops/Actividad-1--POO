5711d14d12651127abccc5f478b90122b96e2c90
JgaravitoG <jgaravitog@unicartagena.edu.co>
2026-04-05 18:21:20 -0500

    Ejercicio 9

--- taller_0/Bloque B/Mascota.java
+++ taller_0/Bloque B/Mascota.java
@@ -6,7 +6,20 @@ class Mascota {
     void mostrarMascota(){
         System.out.println("Nombre: "+nombre);
         System.out.println("Especie: "+especie);
-        System.out.println("edad: "+edad);
+        System.out.println("edad: "+edad+ " años");
+    }
+
+    public static void main(String[] args) {
+        Mascota mascota1 = new Mascota();
+
+        mascota1.nombre = "Firulais";
+        mascota1.especie = "Perro";
+        mascota1.edad = 5;
+        
+        mascota1.mostrarMascota();
+        mascota1.edad = 7;
+        System.out.println("--Cambio edad--");
+        mascota1.mostrarMascota();
     }
     
 }
