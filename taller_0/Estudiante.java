4909a1383e06930a16714dbf0ae654bbbc908577
JgaravitoG <jgaravitog@unicartagena.edu.co>
2026-04-05 14:29:07 -0500

    Ejercicio 1

---
+++ taller_0/Estudiante.java
@@ -0,0 +1,11 @@
+class Estudiante {
+    String nombre;
+    String codigo;
+    int semestre;
+
+    void mostrarInf(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Codigo: "+codigo);
+        System.out.println("Semestre: "+semestre);
+    }
+}
