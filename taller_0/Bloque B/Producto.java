da3c7796218eea311228589e3dd09fa5f92e5268
JgaravitoG <jgaravitog@unicartagena.edu.co>
2026-04-05 18:14:06 -0500

    Ejercicio 8

--- taller_0/Bloque B/Producto.java
+++ taller_0/Bloque B/Producto.java
@@ -9,4 +9,27 @@ public class Producto {
         System.out.println("Stock: "+stock);
     }
     
+    public static void main(String[] args) {
+        Producto producto1 = new Producto();
+        Producto producto2 = new Producto();
+        Producto producto3 = new Producto();
+
+        producto1.nombre = "Arroz";
+        producto1.precio = "$15";
+        producto1.stock = 58;
+
+        producto2.nombre = "Maiz";
+        producto2.precio = "$12";
+        producto2.stock = 74;
+
+        producto3.nombre = "Avena";
+        producto3.precio = "$20";
+        producto3.stock = 62;
+
+        producto1.mostrarProducto();
+        System.out.println("------------");
+        producto2.mostrarProducto();
+        System.out.println("------------");
+        producto3.mostrarProducto();
+    }
 }
