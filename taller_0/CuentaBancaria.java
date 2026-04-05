ef723b48735e05a5709094abe221a6b8b819b0c6
JgaravitoG <jgaravitog@unicartagena.edu.co>
2026-04-05 15:06:05 -0500

    Ejercicio 4

---
+++ taller_0/CuentaBancaria.java
@@ -0,0 +1,12 @@
+class CuentaBancaria {
+    int numero;
+    String titular;
+    float saldo;
+
+    void mostrarCuenta(){
+        System.out.println("Numero: "+numero);
+        System.out.println("Titular: "+titular);
+        System.out.println("Saldo: "+saldo);
+    }
+    
+}
