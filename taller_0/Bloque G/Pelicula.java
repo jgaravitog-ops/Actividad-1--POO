41e43bfe20cca0a71471a86f5fdcf1a791f2f394
JgaravitoG <jgaravitog@unicartagena.edu.co>
2026-04-06 12:04:36 -0500

    Ejercicio 40 Pelicula

---
+++ taller_0/Bloque G/Pelicula.java
@@ -0,0 +1,11 @@
+class Pelicula {
+    String nombre;
+    String genero;
+    String minutos;
+
+    void mostarFicha(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Genero: "+genero);
+        System.out.println("Minutos: "+minutos);
+    }
+}
