1aa27f465db4926bbfcd9613e1f7e1a9f80eef29
JgaravitoG <jgaravitog@unicartagena.edu.co>
2026-04-05 19:29:34 -0500

    Ejercicio 18

---
+++ taller_0/Bloque C/Mascota.java
@@ -0,0 +1,28 @@
+class Mascota {
+    String nombre;
+    String especie;
+    int edad;
+
+    void mostrarMascota(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Especie: "+especie);
+        System.out.println("edad: "+edad+ " años");
+    }
+    void cumplirAnios(){
+        edad += 1;
+    }
+
+    public static void main(String[] args) {
+        Mascota mascota1 = new Mascota();
+
+        mascota1.nombre = "Firulais";
+        mascota1.especie = "Perro";
+        mascota1.edad = 5;
+        
+        mascota1.mostrarMascota();
+        mascota1.edad = 7;
+        System.out.println("--Cambio edad--");
+        mascota1.mostrarMascota();
+    }
+    
+}
