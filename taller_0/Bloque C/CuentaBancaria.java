fd29c28172973aa8a059fc3ce07edb1eaca142fd
JgaravitoG <jgaravitog@unicartagena.edu.co>
2026-04-05 19:08:39 -0500

    Ejercicio 13

---
+++ taller_0/Bloque C/CuentaBancaria.java
@@ -0,0 +1,37 @@
+class CuentaBancaria {
+    int numero;
+    String titular;
+    double saldo;
+
+    void mostrarCuenta(){
+        System.out.println("Numero: "+numero);
+        System.out.println("Titular: "+titular);
+        System.out.println("Saldo: "+saldo);
+    }
+    void consignar(double valor){
+        if (valor>0) {
+            saldo += valor;
+            System.out.println("Se consigno al saldo el siguiente  valor: "+valor);
+        }
+        else{
+            System.out.println("No se puede consignar saldo menor o igual que 0.");
+        }
+    }
+    
+    public static void main(String[] args) {
+        CuentaBancaria cBancaria1 = new CuentaBancaria();
+        CuentaBancaria cBancaria2 = new CuentaBancaria();
+
+        cBancaria1.numero = 123;
+        cBancaria1.titular = "Pedro Gonzales";
+        cBancaria1.saldo = 7769;
+
+        cBancaria2.numero = 321;
+        cBancaria2.titular = "Jose Garcia";
+        cBancaria2.saldo = 5243.5;
+
+        cBancaria1.mostrarCuenta();
+        System.out.println("----------");
+        cBancaria2.mostrarCuenta();
+    }
+}
