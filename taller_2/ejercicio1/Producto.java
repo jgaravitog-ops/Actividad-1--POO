6ede0c07bfd6f6d150f99aa553f3c7424e1cef96
JgaravitoG <jgaravitog@unicartagena.edu.co>
2026-04-14 15:15:05 -0500

    Ejercicio1 taller_2

---
+++ taller_2/ejercicio1/Producto.java
@@ -0,0 +1,15 @@
+public class Producto {
+
+  String nombre;
+  double precio;
+
+  public Producto(String nombre, double precio) {
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+  public void mostarProducto() {
+    System.out.println("Nombre: "+nombre);
+    System.out.println("Precion: "+precio);
+  }
+  
+}
