52d797b9bf77504496be8112da7f72e39edbe38f
JgaravitoG <jgaravitog@unicartagena.edu.co>
2026-04-14 16:34:01 -0500

    Ejercicio 3

--- taller_2/ejercicio3/Casa.java
+++ taller_2/ejercicio3/Casa.java
@@ -3,7 +3,10 @@ package ejercicio3;
 public class Casa {
 
     String color = "azul";
-
+/* 
+        public static void mostarCasa(){     //error al usar this en un metodo static
+        System.out.println(this.color); 
+*/
     public void mostarCasa(){     //la solucion podria ser quitar static del metodo
         System.out.println(this.color); 
     }
